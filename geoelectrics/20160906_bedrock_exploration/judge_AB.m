ee805c48e50473fc4904da0dc9ba5aa7257443d5
SEER2 <asdfghjklqwertyuiop696@gmail.com>
2016-09-08 19:00:33 +0800

    Added more notes in judeg_AB.m

--- geoelectrics/20160906_bedrock_exploration/judge_AB.m
+++ geoelectrics/20160906_bedrock_exploration/judge_AB.m
@@ -1,6 +1,7 @@
 function rho_s=judge_AB(AB, I)
-% AB, I 为数组，对应形式为(AB, I)
-% U为预设值，视情况更改
+% 判断AB间距取值范围，将绘图部分命令注释掉，亦用作visualize的子程序
+% AB, I 为数组，对应形式为(AB, I), 同阶列向量
+% U为预设值，视情况更改.
 
 
 U=11.0; % 视情况更改
@@ -9,6 +10,7 @@ L=AB/2;
 
 rho_s=pi*(L.^2)*U./I;
 
+% 绘图命令
 % plot(L,rho_s);
 % xlabel('AB/2');
 % ylabel('\rho_s');
