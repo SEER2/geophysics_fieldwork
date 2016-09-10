8d05b1a205365fc60fe4924b31e5949c6e1bbbab
SEER2 <asdfghjklqwertyuiop696@gmail.com>
2016-09-10 14:55:14 +0800

    reborn

--- geoelectrics/draw_b_bisect_papers.sh
+++ geoelectrics/draw_b_bisect_papers.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-gmt blockmean b_bisect_xyz_6.txt -I1 -R-16/20/-52/38 > d.xyz
+gmt blockmean b_bisect_xyz_6.txt -I1 -R-16/19/-52/38 > d.xyz
 gmt surface -I1 -R-16/19/-52/38 d.xyz -Gd.grd
-gmt psbasemap -R-16/19/-52/38 -JX3i/5i -Ba10f1/a10f1:."Bedrock Bisect":WSnE -K > bedrock_bisect_AB_6.ps
-gmt grdcontour d.grd -J -B -C5 -A10 -O >> bedrock_bisect_AB_6.ps
+gmt grd2cpt d.grd -Crainbow > d.cpt
+
+gmt grdimage d.grd -JX3i/5i -Ba10f1/a10f1:."Bedrock Bisect":WSnE -Cd.cpt -K > bedrock_bisect_AB_6.ps
+gmt grdcontour d.grd -J -B -C5 -A10 -T0.5i/0.5i:-+ -K -O >> bedrock_bisect_AB_6.ps
+gmt psscale -D1.5i/-0.3i/3i/0.3ch -B10 -Cd.cpt -O >>bedrock_bisect_AB_6.ps
 gmt psconvert -Tj -A -Fbedrock_bisect_AB_6.jpg bedrock_bisect_AB_6.ps
 
 rm d.xyz
 rm d.grd
+rm d.cpt
 rm gmt.history
--- geoelectrics/draw_ua_bisect_papers.sh
+++ geoelectrics/draw_ua_bisect_papers.sh
@@ -2,10 +2,14 @@
 
 gmt blockmean ua_bisect_xyz.txt -I1 -R0/46/0/30 > d.xyz
 gmt surface -I1 -R0/46/0/30 d.xyz -Gd.grd
-gmt psbasemap -R0/46/0/30 -JX5i/3i -Ba10f1/a10f1:."Underground ab Bisect":WSnE -K > ua_bisect.ps
-gmt grdcontour d.grd -J -B -C0.1 -A0.5 -O >> ua_bisect.ps
+gmt grd2cpt d.grd -Crainbow > d.cpt
+
+gmt grdimage d.grd -JX5i/3i -Ba10f1/a10f1:."Underground Abnormity Bisect":WSnE -Cd.cpt -K > ua_bisect.ps
+gmt grdcontour d.grd -J -B -C0.5 -A1 -T0.2i/0.2i:-+ -K -O>> ua_bisect.ps
+gmt psscale -D2.5i/-0.3i/5i/0.3ch -B1 -Cd.cpt -O >> ua_bisect.ps
 gmt psconvert -Tj -A -Fua_bisect.jpg ua_bisect.ps
 
 rm d.xyz
 rm d.grd
+rm d.cpt
 rm gmt.history
